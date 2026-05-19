ae6d6f40b4ffa3084315a6e7b960a25b39432af4
Paolo Meyer <Paolo.Meyer@art.com.au>
2026-05-20 09:26:45 +1000

    chore: stop tracking .next build artifacts

--- .next/dev/static/chunks/src_app_layout_tsx_004glpo._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0fvv3km._.css","static/chunks/node_modules_0fn9zeb._.js","static/chunks/src_0.neb4i._.js"],
-    source: "dynamic"
-});
