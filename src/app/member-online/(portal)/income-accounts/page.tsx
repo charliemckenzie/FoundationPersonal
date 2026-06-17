6f5cfdd5ac3dfac6ceec7a95e13ae4ae8807c8af
connie <connie.huang@art.com.au>
2026-06-17 14:58:51 +1000

    fix: wire retirement income account link to new flow route

--- src/app/member-online/(portal)/income-accounts/page.tsx
+++ src/app/member-online/(portal)/income-accounts/page.tsx
@@ -34,7 +34,7 @@ export default function IncomeAccountsPage() {
               label="Open a Retirement Income Account"
               description="A flexible account that lets you draw down your super as regular income."
               icon="money-simple-from-bracket"
-              href="#"
+              href="/member-online/retirement-income-account"
             />
             <LinkRow
               label="Open a Lifetime Pension"
